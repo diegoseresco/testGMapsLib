482540105fcedc13a7553dd15fb45a7b3f0a2cbb
diego <92336377+diegosalcedov@users.noreply.github.com>
2022-09-19 16:30:05 -0500

    add framework

---
+++ TestSerescoMapsLib/TestSerescoMapsLib.h
@@ -0,0 +1,18 @@
+//
+//  TestSerescoMapsLib.h
+//  TestSerescoMapsLib
+//
+//  Created by diegitsen on 19/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestSerescoMapsLib.
+FOUNDATION_EXPORT double TestSerescoMapsLibVersionNumber;
+
+//! Project version string for TestSerescoMapsLib.
+FOUNDATION_EXPORT const unsigned char TestSerescoMapsLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestSerescoMapsLib/PublicHeader.h>
+
+
